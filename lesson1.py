1f49b34c3a1ce096ce7feb9f1f6e8e9bb6125894
Deniz <146165758+denizguneey@users.noreply.github.com>
2023-10-04 21:06:33 +0300

    add lesson

---
+++ lesson1.py
@@ -0,0 +1,12 @@
+#isim = "deniz"
+
+#mesaj = "merhaba"
+
+#print("{} , {} dedi".format(isim,mesaj))
+#print(f"{isim} {mesaj} dedi")
+
+
+#isim1 = "deniz1"
+#mesaj1 = "deniz2"
+
+#print("{} , {} dedi".format(isim1,mesaj1))
