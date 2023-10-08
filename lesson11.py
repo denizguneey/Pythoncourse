b33e552385a5c15fb6f819911711d999ea50cf2d
Deniz <146165758+denizguneey@users.noreply.github.com>
2023-10-08 19:48:54 +0300

    added lesson

---
+++ lesson11.py
@@ -0,0 +1,36 @@
+#modül çok kullanılan fonksiyonların paketi
+
+# import math
+
+# sonuc = math.factorial(6)
+# print(sonuc)
+
+
+# from math import factorial
+
+# sonuc=factorial(5)
+# print(sonuc)
+
+
+
+# from math import factorial, sqrt
+
+# sonuc=sqrt(5)
+# print(sonuc)
+
+
+
+
+# from math import *
+
+# sonuc=sqrt(5)
+# sonuc=fabs()
+# print(sonuc)
+
+
+
+# def cember_cevresi(r):
+#     return 2*3.14*r
+
+# def daire_alani(r):
+#     return 3.14*r*r  lesson10 da import ettik
