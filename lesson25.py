4b252f1aa7c8e9c9ae025299c8a14fced9263988
Deniz <146165758+denizguneey@users.noreply.github.com>
2023-10-10 21:35:42 +0300

    added lesson

---
+++ lesson25.py
@@ -0,0 +1,44 @@
+#class variables-instance variable
+
+# class calisan:
+#     def __init__(self,isim,maas):
+#         self.isim=isim
+#         self.maas=maas
+
+# calisan1=calisan("Ali",5000)
+# calisan2=calisan("Ahmet",6000)
+
+# print(calisan1.__dict__)
+# print(calisan2.__dict__)
+# print(calisan.__dict__)
+
+
+
+# class calisan:
+#     zam_orani=1.1
+#     def __init__(self,isim,maas):
+#         self.isim=isim
+#         self.maas=maas
+
+# calisan1=calisan("Ali",5000)
+# calisan2=calisan("Ahmet",6000)
+
+# calisan.zam_orani=1.2
+# print(calisan.zam_orani)
+# print(calisan1.zam_orani)
+# print(calisan2.zam_orani)
+
+
+
+# class calisan:
+#     personel_sayisi=0
+#     def __init__(self,isim,maas):
+#         self.isim=isim
+#         self.maas=maas
+#         calisan.personel_sayisi += 1
+
+# print(calisan.personel_sayisi)
+# calisan1=calisan("Ali",5000)
+# print(calisan.personel_sayisi)
+# calisan2=calisan("Ahmet",6000)
+# print(calisan.personel_sayisi)
