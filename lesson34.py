80e85499cc631758922a640899817097ca72a5df
Deniz <146165758+denizguneey@users.noreply.github.com>
2023-10-22 15:43:27 +0300

    added lesson

---
+++ lesson34.py
@@ -0,0 +1,26 @@
+#fonksiyonlara parametre olarak fonksiyon göndermek
+
+# def topla(x,y):
+#     return x+y
+# def carpma(x,y):
+#     return x*y
+# def islem_yap(fonk,a,b):
+#     return fonk(a,b)
+# print(islem_yap(topla,3,5))
+# print(islem_yap(carpma,3,5))
+
+
+
+# liste1=[1,2,3,4,5]
+# liste2=[1,3,4,5,8,9,11]
+
+# def kare_al(x):
+#     return x*x
+# def kup_al(x):
+#     return x**3
+# def map_fonk(fonk,liste):
+#     sonuc=[]
+#     for i in liste:
+#         sonuc.append(fonk(i))
+#     return sonuc
+# print(map_fonk(kup_al,liste1))
